f346a751dbb7604e4d40c2f02ae53a4de543f9d9
dchizhikov <dchizhikov@mail.ru>
2024-08-11 12:40:27 +0300

    Update git_commands.py

--- git_commands.py
+++ git_commands.py
@@ -1,6 +1,6 @@
 import os
 def git_test():
   print("test mir")
-def git_clone2(repo):
+def git_clone(repo):
 #  !git clone $repo #{repo}
-  os.system("git clone $repo")
+  os.system(f"git clone {repo}")
