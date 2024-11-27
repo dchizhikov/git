a8fd6fc5a5813422f12d6416f6271e20b2b1eda6
dchizhikov <dchizhikov@gmail.com>
2024-11-27 09:39:29 +0000

    git 11 commit message

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+current_file_path = __file__
+directory_path = os.path.dirname(current_file_path)
+folder_name = os.path.basename(directory_path)
+
+# Получаем полный путь к родительской директории
+parent_directory_path = os.path.dirname(directory_path)
+
+print("Имя папки:", folder_name)
+print("Полный путь к родительской папке:", parent_directory_path)
+
+
+user_name = "dchizhikov"
+repo_name_git = "git"
+
+repo_up = parent_directory_path
+repo_name = "git" #['git', 'UML', 'databases', 'Telegram', 'tg_bot']
+myRepo = repo_up + '/'+repo_name
+
+repo_url = "https://github.com/"+user_name+"/"+repo_name
+
+public_token = os.environ["public_token"]
+#private_token = os.environ["PAT"]
+_token = public_token #[public_token, private_token]
+
+branch = "origin_479F"
+remote_branch = "main"
