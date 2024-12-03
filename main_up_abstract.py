92b7383479d037ea6b07a09d96b25031e6825004
dchizhikov <dchizhikov@gmail.com>
2024-12-03 12:03:34 +0000

    git test-0.3 commit message

--- main_up_abstract.py
+++ main_up_abstract.py
@@ -1,14 +1,8 @@
 import importlib
 import sys
 
-folder = '/content/git' #['git', 'tg_bot', 'databases', '...']
+folder = '/content/tg_bot' #['git', 'tg_bot', 'databases', '...']
 module = 'main' #['main', 'sqlite.main', '...']
 
 sys.path.append(f'{folder}')
-
-
-#main_sub = importlib.import_module(f"{folder}.{module}")
-#importlib.reload(main_sub)
-tg = importlib.import_module(module)
-
-#tg.test()
+main_sub = importlib.import_module(module)
